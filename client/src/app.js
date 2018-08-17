e764fa826d4f54902bdbe8ef5da2888939befc58
jesus2102 <jesusperezmarin@gmail.com>
2018-08-17 15:25:01 +0100

    can publish list of dinosaurs

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,7 @@ const leaflet = require('leaflet');
 const tilesetKey = require('./helpers/tileset_key.js')
 const TimePeriod = require('./models/time_period.js')
 const TimelineView = require('./views/timeline_view.js')
+const Dinosaur = require('./models/dinosaur.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
@@ -12,6 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
   //   id: 'mapbox.streets',
   //   accessToken: tilesetKey
   // }).addTo(map);
+
+  const dinosaur = new Dinosaur();
+  dinosaur.bindingEvents();
 
   const timelineNav = document.querySelector('nav#timeline');
   const timelineView = new TimelineView(timelineNav);
--- client/src/models/dinosaur.js
+++ client/src/models/dinosaur.js
@@ -2,15 +2,26 @@ const PubSub = require('../helpers/pub_sub.js');
 const RequestHelper = require('../helpers/request.js');
 
 const Dinosaur = function () {
-  this.url = '';
-  this.request = new RequestHelper(this.url);
+  this.url = null;
 };
 
 
+Dinosaur.prototype.bindingEvents = function () {
+  PubSub.subscribe('Timeline:selected-period-ready', (evt) => {
+    const period = evt.detail;
+    console.log(evt.detail);
+    this.url = `https://paleobiodb.org/data1.2/occs/list.json?base_name=dinosauria^aves&show=coords,ident,ecospace,img&idreso=genus&min_ma=${period.lateDate}&max_ma=${period.earlyDate}`;
+    this.request = new RequestHelper(this.url);
+    this.get();
+  })
+};
+
 
 Dinosaur.prototype.get = function () {
-  this.request.get();
+  this.request.get()
     .then((dinosaurs) => {
+      console.log(dinosaurs);
+
       PubSub.publish('Dinosaur:all-dinosaurs-ready', dinosaurs);
     })
     .catch((err) => {
