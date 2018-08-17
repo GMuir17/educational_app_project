6a2d6efeabdd77578bdcd697a389348a9e5c23db
jesus2102 <jesusperezmarin@gmail.com>
2018-08-17 15:35:51 +0100

    filtering dinosaurs data

--- client/src/models/dinosaur.js
+++ client/src/models/dinosaur.js
@@ -2,20 +2,56 @@ const PubSub = require('../helpers/pub_sub.js');
 const RequestHelper = require('../helpers/request.js');
 
 const Dinosaur = function () {
-  this.url = '';
-  this.request = new RequestHelper(this.url);
+  this.url = null;
 };
 
 
+Dinosaur.prototype.bindingEvents = function () {
+  PubSub.subscribe('Timeline:selected-period-ready', (evt) => {
+    const period = evt.detail;
+    console.log(evt.detail);
+    this.url = `https://paleobiodb.org/data1.2/occs/list.json?base_name=dinosauria^aves&show=coords,ident,ecospace,img&idreso=genus&min_ma=${period.lateDate}&max_ma=${period.earlyDate}`;
+    this.request = new RequestHelper(this.url);
+    this.get();
+  })
+};
+
 
 Dinosaur.prototype.get = function () {
-  this.request.get();
+  this.request.get()
     .then((dinosaurs) => {
+      console.log(dinosaurs);
+      // const filterNames = filterByGenusName(dinosaurs.records);
+      const dinosaursData = filterDinosaurData(dinosaurs.records);
+      console.log(dinosaursData);
       PubSub.publish('Dinosaur:all-dinosaurs-ready', dinosaurs);
     })
     .catch((err) => {
       console.error(err);
     })
+}
+
+function filterDinosaurData(dinosaurs) {
+  dinosaurs.forEach((dinosaur) => {
+    dinosaur = {
+      name: dinosaur.tna,
+      longitude: dinosaur.lng,
+      latitude: dinosaur.lat,
+      enviroment: dinosaur.jed,
+      diet: dinosaur.jdt,
+      range: `${dinosaur.eag} - ${dinosaur.lag}`,
+      imageId: dinosaur.img
+    }
+  })
+  return dinosaurs;
+}
+
+// function filterByGenusName(dinosaurs) {
+//   const names = firstname(dinosaurs);
+//   dinosaurs.forEach((dinosaur) => {
+//     if (dinosaur.tna)
+//   })
+
 }
 
 module.exports = Dinosaur;
