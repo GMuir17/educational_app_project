c6d019c6315a7620e315d323d300c7ee14f20ccc
jesus2102 <jesusperezmarin@gmail.com>
2018-08-17 09:56:17 +0100

    get request all dinosaurs created

--- client/src/models/dinosaur.js
+++ client/src/models/dinosaur.js
@@ -2,9 +2,20 @@ const PubSub = require('../helpers/pub_sub.js');
 const RequestHelper = require('../helpers/request.js');
 
 const Dinosaur = function () {
+  this.url = '';
+  this.request = new RequestHelper(this.url);
+};
 
 
 
+Dinosaur.prototype.get = function () {
+  this.request.get();
+    .then((dinosaurs) => {
+      PubSub.publish('Dinosaur:all-dinosaurs-ready', dinosaurs);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
 }
 
 module.exports = Dinosaur;
