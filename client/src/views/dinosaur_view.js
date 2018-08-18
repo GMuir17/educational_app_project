5bab71fd9e0a3ae092945055755cb59efc7a4546
Wil Cornish <wilcoclahas@gmail.com>
2018-08-18 16:46:09 +0100

    adds method to render dinosaur view

--- client/src/views/dinosaur_view.js
+++ client/src/views/dinosaur_view.js
@@ -4,13 +4,6 @@ const DinosaurView = function (element) {
   this.element = element;
 }
 
-const selectedDino = {
-  name: "BananaRex",
-  description: "A very big banana",
-  imageURL: "images/feathered-t-rex1.jpg"
-  facts: 'Allosaurus was a large bipedal predator. Its skull was large and equipped with dozens of sharp, serrated teeth. It averaged 8.5 m (28 ft) in length, though fragmentary remains suggest it could have reached over 12 m (39 ft). Relative to the large and powerful hindlimbs, its three-fingered forelimbs were small, and the body was balanced by a long and heavily muscled tail.'
-}
-
 DinosaurView.prototype.bindEvents = function () {
   PubSub.subscribe(('', evt) => {
     this.render(evt.detail);
@@ -58,6 +51,5 @@ dinosaurView.prototype.createFactFile = function (facts) {
   facts.id = 'fact-file'
   facts.textContent = '${facts}'
 };
-
 
 module.exports = dinosaurView;
--- client/src/views/timeline_view.js
+++ client/src/views/timeline_view.js
@@ -24,6 +24,12 @@ Timeline.prototype.renderTimeline = function (periods)  {
       console.log('all periods in list: ', evt.target.value);
       const selectedPeriod = evt.target.value;
       PubSub.publish('TimelineMenu:selected-period', selectedPeriod);
+      const selectedDino = {
+        name: "BananaRex",
+        description: "A very big banana",
+        imageURL: "images/feathered-t-rex1.jpg",
+        facts: 'Allosaurus was a large bipedal predator. Its skull was large and equipped with dozens of sharp, serrated teeth. It averaged 8.5 m (28 ft) in length, though fragmentary remains suggest it could have reached over 12 m (39 ft). Relative to the large and powerful hindlimbs, its three-fingered forelimbs were small, and the body was balanced by a long and heavily muscled tail.'}
+      PubSub.publish('TimelineMenu:fake-dino', selectedDino)
     })
   })
   this.timeline.appendChild(listOfPeriods);
