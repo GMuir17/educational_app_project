05801deef988bd84c25b144a0a9e6b2dab8bd2a1
jesus2102 <jesusperezmarin@gmail.com>
2018-08-18 13:03:12 +0100

    filter families created v2

--- client/src/views/diet_view.js
+++ client/src/views/diet_view.js
@@ -8,7 +8,7 @@ DietView.prototype.bindingEvents = function () {
   PubSub.subscribe('Dinosaur:all-dinosaurs-ready', (evt) => {
     const dinosaurs = evt.detail;
     const UniqueDiets = getUniqueDiets(dinosaurs);
-    // this.renderTags(dinosaurs);
+    this.renderTags(uniqueDiets);
   })
 };
 
@@ -28,16 +28,18 @@ function getUniqueDiets(dinosaurs) {
   return filteredDiets;
 }
 
-// DietView.prototype.renderTags = function (dinosaurs) {
-//   const tagsList = document.createElement('ul');
-//     const all = document.createElement('li');
-//     all.textContent = dinosaurs[0].period;
-//     all.value = -1;
-//   tagsList.appendChild(all);
-//   dinosaurs.forEach((dinosaur) => {
-//     const item = document.createElement('li');
-//     item.textContent
-//   })
+DietView.prototype.renderTags = function (dinosaurs) {
+  const tagsList = document.createElement('ul');
+    const all = document.createElement('li');
+    all.textContent = dinosaurs[0].period;
+    all.value = -1;
+  tagsList.appendChild(all);
+  dinosaurs.forEach((dinosaur) => {
+    if(dinosaur.diet === 'carnivore' || dinosaur.diet === 'herbivore' || dinosaur.diet === 'omnivore')
+    const item = document.createElement('li');
 
-// };
+    item.textContent
+  })
+};
+
 module.exports = DietView;
