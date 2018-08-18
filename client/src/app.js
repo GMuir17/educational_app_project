f49216edb5f7753d4a4c91f8489835399a3b4bde
Wil Cornish <wilcoclahas@gmail.com>
2018-08-18 14:54:18 +0100

    resolves merge fully

--- client/src/app.js
+++ client/src/app.js
@@ -9,19 +9,9 @@ const Map = require('./models/map.js')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
-<<<<<<< HEAD
 
   const dinosaur = new Dinosaur();
   dinosaur.bindingEvents();
-=======
-  // const map = L.map('map').setView([55.9472, -3.2017], 13);
-  // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //   maxZoom: 18,
-  //   id: 'mapbox.streets',
-  //   accessToken: tilesetKey
-  // }).addTo(map);
->>>>>>> develop
 
   const timelineNav = document.querySelector('#timeline-container');
   const timelineView = new TimelineView(timelineNav);
