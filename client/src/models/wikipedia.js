5abef29525359215c82c232b7e6bd5f1c8cbc141
Chris Craig <ctc2@live.co.uk>
2018-08-18 14:46:01 +0100

    before mergin develop

--- client/src/models/wikipedia.js
+++ client/src/models/wikipedia.js
@@ -3,12 +3,25 @@ const RequestHelper = require('../helpers/request.js');
 
 const Wikipedia = function () {
   this.url = '';
-  this.request = new RequestHelper(this.url);
+  this.dinosaurs = null;
 };
 
 
 Wikipedia.prototype.bindingEvents = function () {
   PubSub.subscribe(`Dinosaur:all-dinosaurs-ready`, (evt) => {
+    this.dinosaurs = evt.detail
+     this.dinosaurs.forEach((dinosaur) => {
+       this.dinosaur.name = evt.newName;
+    }.then() =>{
+      this.url = `https://en.wikipedia.org/w/api.php?action=query&titles=${this.dinosaur.name}&format=json&prop=pageimages|extracts`
+        const request = new Request(url)
+        request.get()
+        .then((data) =>{
+          this.imageId = evt.imageId
+          this.url = `https://en.wikipedia.org/w/api.php?action=query&titles=${this.imageId}&format=json&prop=pageimages|extracts`
+        })
+     }
+
 
   })
 }
