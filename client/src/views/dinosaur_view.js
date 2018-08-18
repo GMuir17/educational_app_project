0f0b660dc4b3151a3045c3322c3146c0a604a577
Wil Cornish <wilcoclahas@gmail.com>
2018-08-18 16:32:18 +0100

    adds fact file view

--- client/src/views/dinosaur_view.js
+++ client/src/views/dinosaur_view.js
@@ -8,6 +8,7 @@ const selectedDino = {
   name: "BananaRex",
   description: "A very big banana",
   imageURL: "images/feathered-t-rex1.jpg"
+  facts: 'Allosaurus was a large bipedal predator. Its skull was large and equipped with dozens of sharp, serrated teeth. It averaged 8.5 m (28 ft) in length, though fragmentary remains suggest it could have reached over 12 m (39 ft). Relative to the large and powerful hindlimbs, its three-fingered forelimbs were small, and the body was balanced by a long and heavily muscled tail.'
 }
 
 DinosaurView.prototype.bindEvents = function () {
@@ -53,7 +54,9 @@ DinosaurView.prototype.createImage = function (image) {
 };
 
 dinosaurView.prototype.createFactFile = function (facts) {
-
+  const facts = document.createElement('p');
+  facts.id = 'fact-file'
+  facts.textContent = '${facts}'
 };
 
 
