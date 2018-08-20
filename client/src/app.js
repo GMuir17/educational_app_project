4b8cd322b065d74d9530281fbee14ae1fbf020d0
Wil Cornish <wilcoclahas@gmail.com>
2018-08-20 11:52:54 +0100

    adds randomiser for hero image on page load

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,24 @@
 const leaflet = require('leaflet');
+
 const tilesetKey = require('./helpers/tileset_key.js');
-const TimePeriod = require('./models/time_period.js');
+const HeroImages = require('./helpers/hero_images.js');
+
 const TimelineView = require('./views/timeline_view.js');
 const DietView = require('./views/diet_view.js');
-const Diet = require('./models/diet.js');
-const Dinosaur = require('./models/dinosaur.js');
-const Map = require('./models/map.js')
 const DinosaurView = require('./views/dinosaur_view.js')
 const TimePeriodView = require('./views/time_period_view.js');
 
+const TimePeriod = require('./models/time_period.js');
+const Diet = require('./models/diet.js');
+const Dinosaur = require('./models/dinosaur.js');
+const Map = require('./models/map.js')
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+
+  const imageContainer = document.querySelector('.hero-image')
+  const heroImages = new HeroImages(imageContainer);
+  heroImages.showRandomImage();
+
 
   const timelineNav = document.querySelector('#timeline-container');
   const timelineView = new TimelineView(timelineNav);
