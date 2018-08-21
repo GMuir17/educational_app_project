d6e42ad614410d2f7becfeace3348e67d7fc3a4f
jesus2102 <jesusperezmarin@gmail.com>
2018-08-21 12:24:41 +0100

    refactor attempted v1.64

--- client/src/views/time_period_view.js
+++ client/src/views/time_period_view.js
@@ -7,15 +7,13 @@ const TimePeriodView = function (container) {
   this.mainContainer = null;
   this.periodDescription = null;
   this.nav = null;
-  // this.createDinosaurViewOnClick = this.createDinosaurViewOnClick.bind(this);
-  // this.removeDinosaurViewOnClick = this.removeDinosaurViewOnClick.bind(this);
 };
 
 TimePeriodView.prototype.bindEvents = function () {
 
 
-  PubSub.subscribe('Wikipedia:period-data-ready', (event) => {
-    const periodDescription = event.detail;
+  PubSub.subscribe('Wikipedia:period-data-ready', (evt) => {
+    const periodDescription = evt.detail;
     console.log("break");
     this.mainContainer = this.createMain();
     this.container.appendChild(this.mainContainer);
@@ -32,50 +30,41 @@ TimePeriodView.prototype.bindEvents = function () {
 
   });
 
+  PubSub.subscribe('Wikipedia:all-dinosaurs-ready', (evt) => {
+    const lightbox = document.createElement('div')
+    lightbox.classList = 'time-period-lightbox'
+    this.container.appendChild(lightbox)
+    lightbox.addEventListener('click', () => {
+      this.container.innerHTML = '';
+    });
 
-//   // TODO: make sure this channel matches with the wiki view
-//   PubSub.subscribe('Wikipedia:all-dinosaurs-ready', (evt) => {
-//     this.container.innerHTML = '';
-//
-//     const lightbox = document.createElement('div')
-//     lightbox.classList = 'time-period-lightbox'
-//     this.container.appendChild(lightbox)
-//     lightbox.addEventListener('click', () => {
-//       this.container.innerHTML = '';
-//     });
-//
-//     // this.mainContainer = this.createMain();
-//     // this.container.appendChild(this.mainContainer);
-//
-    // const dinosaurs = null;
-    // this.render();
-//
-//
-//   });
+    const dinosaurs = evt.detail;
+    this.render(dinosaurs);
+
+
+  });
 };
-//
-// TimePeriodView.prototype.removeTimePeriodViewOnClick = function () {
-//   PubSub.publish('TimePeriodView:exit-click');
-// };
-//
-// TimePeriodView.prototype.createDinosaurViewOnClick = function (evt) {
-//   const selectedDino = evt.target.value;
-//   PubSub.publish('TimePeriodView:dinosaur-selected', selectedDino);
-// };
 
-// TimePeriodView.prototype.render = function (dinosaurs) {
-//   this.renderDescription();
-// //   dinosaurs.forEach((dinosaur) => {
-// //     // TODO: should I use "article.dinosaur-preview" here?
-// //     const article = document.createElement('article');
-// //     article.classList.add("dinosaur-preview");
-// //     const dinosaurPreviewView = new DinosaurPreviewView(article, dinosaur);
-// //     dinosaurPreviewView.makeEventListener();
-// //     dinosaurPreviewView.render()
-// //     this.mainContainer.appendChild(article);
-// //     article.addEventListener('click', this.createDinosaurViewOnClick);
-// //   });
-// };
+TimePeriodView.prototype.removeTimePeriodViewOnClick = function () {
+  PubSub.publish('TimePeriodView:exit-click');
+};
+
+TimePeriodView.prototype.createDinosaurViewOnClick = function (evt) {
+  const selectedDino = evt.target.value;
+  PubSub.publish('TimePeriodView:dinosaur-selected', selectedDino);
+};
+
+TimePeriodView.prototype.render = function (dinosaurs) {
+  dinosaurs.forEach((dinosaur) => {
+    const article = document.createElement('article');
+    article.classList.add("dinosaur-preview");
+    const dinosaurPreviewView = new DinosaurPreviewView(article, dinosaur);
+    dinosaurPreviewView.makeEventListener();
+    dinosaurPreviewView.render()
+    this.mainContainer.appendChild(article);
+    article.addEventListener('click', this.createDinosaurViewOnClick);
+  });
+};
 
 TimePeriodView.prototype.renderDescription = function (periodDescription) {
 
@@ -95,8 +84,6 @@ TimePeriodView.prototype.createMain = function () {
 };
 
 TimePeriodView.prototype.renderDietTabs = function (diets) {
-
-  // this.nav.innerHTML = '';
   const dietView = new DietView(this.nav, diets);
   dietView.bindEvents();
   this.mainContainer.appendChild(this.nav);
